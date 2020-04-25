7b656210e5db4ab4e9fe0943d9d0ee5a48a35cb5
Farukh-K <kutlikov.farukh@gmail.com>
2020-04-25 11:33:45 +0600

    Добавление чека

---
+++ database/migrations/2020_04_25_052222_create_strings_of_checks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStringsOfChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('strings_of_checks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('check_id');
+            $table->unsignedBigInteger('product_id');
+            $table->decimal('amount_of_product_in_check', 5, 2);
+            $table->timestamps();
+
+
+            $table->index('check_id');
+            $table->index('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('strings_of_checks');
+    }
+}
